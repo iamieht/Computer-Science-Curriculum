c50730e1685dfdaa0aaec7510acb96fdc42b578c
Iván E. Hernández T <iamieht@gmail.com>
2019-09-04 19:11:18 +0200

    Week1: Problem Set 1 -> Problem 3 fixed

--- MITx-6.00.1x/Week1/Problem3.py
+++ MITx-6.00.1x/Week1/Problem3.py
@@ -12,16 +12,16 @@ longest = ''
 temp = s[0]
 
 for idx in range(len(s)-1):
-#    if s[idx] <= s[idx+1]:
     if s[idx+1] >= s[idx]:
         temp += s[idx+1]
-#        print(temp)
+        if len(temp) > len(longest):
+            longest = temp
     else:
         if len(temp) > len(longest):
             longest = temp
-            temp = ''
+            temp = s[idx+1]
         else:
-            temp = ''
+            temp = s[idx+1]
 
 print("Longest substring in alphabetical order is:", longest)
 
